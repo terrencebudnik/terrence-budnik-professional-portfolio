70cdf20a18d566c875d1ceac42d64797ccdb0049
terrence <terrence.budnik@gmail.com>
2022-11-21 17:57:09 -0600

    added navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,18 @@ export default function Header() {
  
 
   return (
+    <>
     <div className='header'> 
           <h1 className='animation'>Terrence Budnik</h1>
-          
     </div>
+    
+    <div className='navbar'>
+      <button className='navItems'>About Me</button>
+      <button className='navItems'>Contact Info</button>
+      <button className='navItems'>Resume</button>
+      <button className='navItems'>Links</button>
+    </div>
+    </>
       
   );
 }
